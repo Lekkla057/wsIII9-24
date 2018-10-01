3bad500aac7bc1ba98624a6b2b1ed2a12f4411ab
Noppadon kaewkong <ntkotan019@gmail.com>
2018-10-01 16:08:54 +0700

    giy

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require('express');
 var pgp = require('pg-promise')();
 var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 var app = express();
+var bodyParser = require('body-parser');//บังคับ
+app.use(bodyParser.json());//บังคับ
+app.use(bodyParser.urlencoded({ extended: true })); //บังคับ
+
+
+
+
 // app.use(express.static('static'));
 app.set('view engine','ejs');
 app.get('/', function(req, res) {
@@ -90,6 +97,17 @@ app.get('/users/:id', function(req, res) {
     
 });
 
+
+//update
+app.post('/products/update',function (req, res) {
+var id =req.body.id;
+var title =req.body.title;
+var price =req.body.price;
+var sql=`update products set title=${title},price=${price} where id=${id}`;
+//db.none
+console.log('Update'+sql)
+res.redirect('/products');
+})
 
 
 console.log('app is running at http://localhost:8080');
