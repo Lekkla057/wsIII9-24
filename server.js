a62bea3139dd450d74f84c722af088210dcc424d
Noppadon kaewkong <ntkotan019@gmail.com>
2018-10-18 03:15:49 +0700

    edit product4.1

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ var id =req.body.id;
 var title =req.body.title;
 var price =req.body.price;
 var sql=`update products set title=${title},price=${price} where id=${id}`;
-db.any(sql)
+db.any(sql,)
 .then(function(data){
     console.log('DATA:'+data);
    
